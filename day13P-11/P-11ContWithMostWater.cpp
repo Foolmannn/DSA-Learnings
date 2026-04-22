f33b428f7885a0325907ef7c7b55898321840988
Suman Pun Magar <mannmagar19@gmail.com>
2026-04-22 17:19:23 +0545

    Problemm 11 two pointer approach cleaner and better

--- day13P-11/P-11ContWithMostWater.cpp
+++ day13P-11/P-11ContWithMostWater.cpp
@@ -68,3 +68,24 @@ public:
         return area;  // return the maximum area found
     }
 };
+
+// Cleaner and better with same two pointer approach 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int area = 0;
+
+        while (left < right) {
+            area = max(area, min(height[left], height[right]) * (right - left));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return area;
+    }
+};
